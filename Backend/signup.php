02796f9c15fffe5560bc91b050a096559f260383
Ryunosuke500 <70992113+Ryunosuke500@users.noreply.github.com>
2020-10-27 06:23:55 +0900

    Add files via upload
    First, open sigun_up.php for temp register, it create link to signup.php, then register to DB

---
+++ Backend/signup.php
@@ -0,0 +1,209 @@
+<?php
+session_start();
+//for CSRF
+$_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
+$token = $_SESSION['token'];
+//for click jacking
+header('X-FRAME-OPTIONS: SAMEORIGIN');
+
+//init error message
+$errors = array();
+
+//DB info
+// we can create new php file to connect DB later
+$user = 'root';
+$password = 'rmpoke1945';
+$dbName = "student_profile";
+$host = "localhost";
+//DB connection
+$dsn = "mysql:host={$host};dbname={$dbName};charser=utf8mb4";
+$pdo = new PDO($dsn, $user, $password);
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if(empty($_GET)) {
+	header("Location: registration_mail");
+	exit();
+}else{
+	//insert get data
+	$urltoken = isset($_GET["urltoken"]) ? $_GET["urltoken"] : NULL;
+	//check token
+	if ($urltoken == ''){
+		$errors['urltoken'] = "No token.";
+	}else{
+		try{
+			// connect to DB
+			//flag = 0, it means temp member
+			$sql = "SELECT mail FROM pre_user WHERE urltoken=(:urltoken) AND flag =0 AND date > now() - interval 24 hour";
+           $stm = $pdo->prepare($sql);
+			$stm->bindValue(':urltoken', $urltoken, PDO::PARAM_STR);
+			$stm->execute();
+			
+			//get record num
+			$row_count = $stm->rowCount();
+			
+			// within 24hrs and temp member token
+			if( $row_count ==1){
+				$mail_array = $stm->fetch();
+				$mail = $mail_array["mail"];
+				$_SESSION['mail'] = $mail;
+			}else{
+				$errors['urltoken_timeover'] = "
+                This URL is not available. The expiration date may have expired or the URL may be incorrect. Please register again.";
+			}
+			//check db connection
+			$stm = null;
+		}catch (PDOException $e){
+			print('Error:'.$e->getMessage());
+			die();
+		}
+	}
+}
+
+/**
+* btn_confirm
+*/
+if(isset($_POST['btn_confirm'])){
+	if(empty($_POST)) {
+		header("Location: registration_mail.php");
+		exit();
+	}else{
+		//POST data
+		$name = isset($_POST['name']) ? $_POST['name'] : NULL;
+        $password = isset($_POST['password']) ? $_POST['password'] : NULL;
+        $role = isset($_POST['role']) ? $_POST['role'] : NULL;
+        $role_1 = $role;
+		
+		//session
+		$_SESSION['name'] = $name;
+        $_SESSION['password'] = $password;
+        $_SESSION['role'] = $role;
+        if($role_1==='0'){
+            $role_1 = 'Student';
+        }else $role_1 = 'Professor';
+
+		//account
+		//password
+		if ($password == ''):
+			$errors['password'] = "No password entered";
+		else:
+			$password_hide = str_repeat('*', strlen($password));
+		endif;
+
+		if ($name == ''):
+			$errors['name'] = "No name entered";
+        endif;
+        
+        if ($role == ''):
+            $errors['role'] = "No role selected";
+		endif;
+	}
+	
+}
+
+/**
+* page_3
+* btn_submit
+*/
+if(isset($_POST['btn_submit'])){
+	//hash password
+	$password_hash =  password_hash($_SESSION['password'], PASSWORD_DEFAULT);
+
+	//register to DB here
+	try{
+		$sql = "INSERT INTO user_datas (name,password,mail,status,created_at,updated_at,role) VALUES (:name,:password_hash,:mail,0,now(),now(),:role)";
+       $stm = $pdo->prepare($sql);
+		$stm->bindValue(':name', $_SESSION['name'], PDO::PARAM_STR);
+        $stm->bindValue(':mail', $_SESSION['mail'], PDO::PARAM_STR);
+        $stm->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+        $stm->bindValue(':role', $_SESSION['role'], PDO::PARAM_STR);
+		$stm->execute();
+
+		//pre_user flag -> 1, invalidation token
+		$sql = "UPDATE pre_user SET flag=1 WHERE mail=:mail";
+		$stm = $pdo->prepare($sql);
+		//set the actual value in the placeholder
+		$stm->bindValue(':mail', $mail, PDO::PARAM_STR);
+		$stm->execute();
+						
+		/*
+		* sending temporary registered emails to registered users and admin
+       */
+/* 
+		$mailTo = $mail.','.$companymail;
+       $body = <<< EOM
+       Thank you for registering.
+EOM;
+       mb_language('en');
+       mb_internal_encoding('UTF-8');
+   
+       //From header
+       $header = 'From: ' . mb_encode_mimeheader($companyname). ' <' . $companymail. '>';
+   
+       if(mb_send_mail($mailTo, $registation_mail_subject, $body, $header, '-f'. $companymail)){          
+           $message['success'] = "Registered!";
+       }else{
+           $errors['mail_error'] = "Failed to send email.";
+		}	
+*/
+		//check DB connection
+		$stm = null;
+
+		//Release all session value
+		$_SESSION = array();
+		//Delete all cookie
+		if (isset($_COOKIE["PHPSESSID"])) {
+				setcookie("PHPSESSID", '', time() - 1800, '/');
+		}
+		//destroy session
+		session_destroy();
+
+	}catch (PDOException $e){
+		//Transaction cancellation (rollback)
+		$pdo->rollBack();
+		$errors['error'] = "Please try again";
+		print('Error:'.$e->getMessage());
+	}
+}
+
+?>
+
+<h1>Member registration</h1>
+
+<!-- page_3 Complete page-->
+<?php if(isset($_POST['btn_submit']) && count($errors) === 0): ?>
+Registered.
+
+<!-- page_2 Confirm page-->
+<?php elseif (isset($_POST['btn_confirm']) && count($errors) === 0): ?>
+	<form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>?urltoken=<?php print $urltoken; ?>" method="post">
+		<p>Email address：<?=htmlspecialchars($_SESSION['mail'], ENT_QUOTES)?></p>
+		<p>Password：<?=$password_hide?></p>
+        <p>Name：<?=htmlspecialchars($name, ENT_QUOTES)?></p>
+        <p>Your role: <?=$role_1?></p>
+		
+		<input type="submit" name="btn_back" value="Back">
+		<input type="hidden" name="token" value="<?=$_POST['token']?>">
+		<input type="submit" name="btn_submit" value="Register">
+	</form>
+
+<?php else: ?>
+<!-- page_1 Register page -->
+	<?php if(count($errors) > 0): ?>
+       <?php
+       foreach($errors as $value){
+           echo "<p class='error'>".$value."</p>";
+       }
+       ?>
+   <?php endif; ?>
+		<?php if(!isset($errors['urltoken_timeover'])): ?>
+			<form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>?urltoken=<?php print $urltoken; ?>" method="post">
+				<p>Email address：<?=htmlspecialchars($mail, ENT_QUOTES, 'UTF-8')?></p>
+				<p>Password：<input type="password" name="password"></p>
+                <p>Name：<input type="text" name="name" value="<?php if( !empty($_SESSION['name']) ){ echo $_SESSION['name']; } ?>"></p>
+                <p>Student/Professor?<input type="radio" name ="role" value="0">Student<input type="radio" name ="role" value="1">Professor</p>
+				<input type="hidden" name="token" value="<?=$token?>">
+				<input type="submit" name="btn_confirm" value="Confirm">
+			</form>
+		<?php endif ?>
+<?php endif; ?>
---
+++ Backend/signup_mail.php
@@ -0,0 +1,121 @@
+<?php
+session_start();
+//（CSRF)
+$_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
+$token = $_SESSION['token'];
+// Clickjacking
+header('X-FRAME-OPTIONS: SAMEORIGIN');
+// database information !!!create DBconnect php file later
+$user = 'root';
+$password = 'rmpoke1945';
+$dbName = "student_profile";
+$host = "localhost";
+//init error message
+$errors = array();
+//connect to DB
+$dsn = "mysql:host={$host};dbname={$dbName};charser=utf8mb4";
+$pdo = new PDO($dsn, $user, $password);
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//after click "send" button
+if (isset($_POST['submit'])) {
+   //if email is empty
+   if (empty($_POST['mail'])) {
+       $errors['mail'] = 'Email address has not been entered.';
+   }else{
+       //put post data into variable
+       $mail = isset($_POST['mail']) ? $_POST['mail'] : NULL;
+   
+       //chack mail address
+       if(!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $mail)){
+			$errors['mail_check'] = "Email address format is incorrect.";
+       }
+       //DB check        
+       $sql = "SELECT id FROM user_datas WHERE mail=:mail";
+       $stm = $pdo->prepare($sql);
+       $stm->bindValue(':mail', $mail, PDO::PARAM_STR);
+       
+       $stm->execute();
+       $result = $stm->fetch(PDO::FETCH_ASSOC);
+       //if same value is in user_data, throw error
+       if(isset($result["id"])){
+			$errors['user_check'] = "This email address is already in use.";
+       }
+       
+   }
+   //if no error, insert data to pre_user table
+   if (count($errors) === 0){
+       $urltoken = hash('sha256',uniqid(rand(),1));
+       // Caution!!
+       //we change url when we use AWS
+       // Caution!!
+       $url = "http://localhost/kraken/signup.php?urltoken=".$urltoken;
+       //register to db here
+       try{
+           //exception
+           $sql = "INSERT INTO pre_user (urltoken, mail, date, flag) VALUES (:urltoken, :mail, now(), '0')";
+           $stm = $pdo->prepare($sql);
+           $stm->bindValue(':urltoken', $urltoken, PDO::PARAM_STR);
+           $stm->bindValue(':mail', $mail, PDO::PARAM_STR);
+           $stm->execute();
+           $pdo = null;
+           $message = "sent you an email. Please register from the URL provided in the email within 24 hours.";     
+       }catch (PDOException $e){
+           print('Error:'.$e->getMessage());
+           die();
+       }
+       /*
+       * Email certificate
+       * now I test without this
+       * 
+       */
+       /*  
+   	$mailTo = $mail;
+       $body = <<< EOM
+       register below URL
+       within 24 hours
+       {$url}
+EOM;
+       mb_language('en');
+       mb_internal_encoding('UTF-8');
+   
+       //create from header
+       $header = 'From: ' . mb_encode_mimeheader($companyname). ' <' . $companymail. '>';
+   
+       if(mb_send_mail($mailTo, $registation_subject, $body, $header, '-f'. $companymail)){      
+           //release session
+           $_SESSION = array();
+           //delete cookie
+           if (isset($_COOKIE["PHPSESSID"])) {
+               setcookie("PHPSESSID", '', time() - 1800, '/');
+           }
+           //destroy session
+           session_destroy();
+           $message = "
+Sent you an email. Please register from the URL provided in the email within 24 hours.";
+       }
+       */
+   }
+}
+?>
+<h1>Temporary membership registration</h1>
+<?php if (isset($_POST['submit']) && count($errors) === 0): ?>
+   <!-- registered  -->
+   <p><?=$message?></p>
+   <p>↓TEST(delete this later)：sent email has this URL</p>
+   <a href="<?=$url?>"><?=$url?></a>
+<?php else: ?>
+<!-- register -->
+   <?php if(count($errors) > 0): ?>
+       <?php
+       foreach($errors as $value){
+           echo "<p class='error'>".$value."</p>";
+       }
+       ?>
+   <?php endif; ?>
+   <form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>" method="post">
+       <p>email address：<input type="text" name="mail" size="50" value="<?php if( !empty($_POST['mail']) ){ echo $_POST['mail']; } ?>"></p> 
+       <input type="hidden" name="token" value="<?=$token?>">
+       <input type="submit" name="submit" value="Send">
+   </form>
+<?php endif; ?>
