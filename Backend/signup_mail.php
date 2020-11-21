545d5b81deacbc63e8e6b1ce98d63cfad8d8824c
austinD14 <austindimalanta14@gmail.com>
2020-11-20 22:14:59 -0800

    student can now update their info and see it in their main page

--- Backend/signup_mail.php
+++ Backend/signup_mail.php
@@ -19,58 +19,57 @@ $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //after click "send" button
 if (isset($_POST['submit'])) {
-   //if email is empty
-   if (empty($_POST['mail'])) {
-       $errors['mail'] = 'Email address has not been entered.';
-   }else{
-       //put post data into variable
-       $mail = isset($_POST['mail']) ? $_POST['mail'] : NULL;
-   
-       //chack mail address
-       if(!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $mail)){
-			$errors['mail_check'] = "Email address format is incorrect.";
-       }
-       //DB check        
-       $sql = "SELECT id FROM user_data WHERE mail=:mail";
-       $stm = $pdo->prepare($sql);
-       $stm->bindValue(':mail', $mail, PDO::PARAM_STR);
-       
-       $stm->execute();
-       $result = $stm->fetch(PDO::FETCH_ASSOC);
-       //if same value is in user_data, throw error
-       if(isset($result["id"])){
-			$errors['user_check'] = "This email address is already in use.";
-       }
-       
-   }
-   //if no error, insert data to pre_user table
-   if (count($errors) === 0){
-       $urltoken = hash('sha256',uniqid(rand(),1));
-       // Caution!!
-       //we change url when we use AWS
-       // Caution!!
-       $url = "http://localhost/kraken/signup.php?urltoken=".$urltoken;
-       //register to db here
-       try{
-           //exception
-           $sql = "INSERT INTO pre_user (urltoken, mail, date, flag) VALUES (:urltoken, :mail, now(), '0')";
-           $stm = $pdo->prepare($sql);
-           $stm->bindValue(':urltoken', $urltoken, PDO::PARAM_STR);
-           $stm->bindValue(':mail', $mail, PDO::PARAM_STR);
-           $stm->execute();
-           $pdo = null;
-           $message = "The email was sent. Please register from the URL provided in the email within 24 hours.";     
-       }catch (PDOException $e){
-           print('Error:'.$e->getMessage());
-           die();
-       }
-       /*
+    //if email is empty
+    if (empty($_POST['mail'])) {
+        $errors['mail'] = 'Email address has not been entered.';
+    } else {
+        //put post data into variable
+        $mail = isset($_POST['mail']) ? $_POST['mail'] : NULL;
+
+        //chack mail address
+        if (!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $mail)) {
+            $errors['mail_check'] = "Email address format is incorrect.";
+        }
+        //DB check        
+        $sql = "SELECT id FROM user_data WHERE mail=:mail";
+        $stm = $pdo->prepare($sql);
+        $stm->bindValue(':mail', $mail, PDO::PARAM_STR);
+
+        $stm->execute();
+        $result = $stm->fetch(PDO::FETCH_ASSOC);
+        //if same value is in user_data, throw error
+        if (isset($result["id"])) {
+            $errors['user_check'] = "This email address is already in use.";
+        }
+    }
+    //if no error, insert data to pre_user table
+    if (count($errors) === 0) {
+        $urltoken = hash('sha256', uniqid(rand(), 1));
+        // Caution!!
+        //we change url when we use AWS
+        // Caution!!
+        $url = "http://localhost/kraken/signup.php?urltoken=" . $urltoken;
+        //register to db here
+        try {
+            //exception
+            $sql = "INSERT INTO pre_user (urltoken, mail, date, flag) VALUES (:urltoken, :mail, now(), '0')";
+            $stm = $pdo->prepare($sql);
+            $stm->bindValue(':urltoken', $urltoken, PDO::PARAM_STR);
+            $stm->bindValue(':mail', $mail, PDO::PARAM_STR);
+            $stm->execute();
+            $pdo = null;
+            $message = "The email was sent. Please register from the URL provided in the email within 24 hours.";
+        } catch (PDOException $e) {
+            print('Error:' . $e->getMessage());
+            die();
+        }
+        /*
        * Email certificate
        * now I test without this
        * This email function might not work on localhost without SMTP server
        * We need to find out the way of email certificate on AWS service
        */
-       /*  
+        /*  
        $mailTo = $mail;
        
        //Return-Path address
@@ -104,27 +103,53 @@ EOM;
            $errors['mail_error'] = "Failed to send email.";
        }
        */
-   }
+    }
 }
 ?>
-<h1>Temporary membership registration</h1>
-<?php if (isset($_POST['submit']) && count($errors) === 0): ?>
-   <!-- registered  -->
-   <p><?=$message?></p>
-   <p>↓TEST(delete this later)：sent email has this URL</p>
-   <a href="<?=$url?>"><?=$url?></a>
-<?php else: ?>
-<!-- register -->
-   <?php if(count($errors) > 0): ?>
-       <?php
-       foreach($errors as $value){
-           echo "<p class='error'>".$value."</p>";
-       }
-       ?>
-   <?php endif; ?>
-   <form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>" method="post">
-       <p>email address：<input type="text" name="mail" size="50" value="<?php if( !empty($_POST['mail']) ){ echo $_POST['mail']; } ?>"></p> 
-       <input type="hidden" name="token" value="<?=$token?>">
-       <input type="submit" name="submit" value="Send">
-   </form>
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Student Profile</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.0/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="Styles/site.css"" />
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class=" navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand">Student Profile</a>
+    </nav>
+    <div style="height: 80px"></div>
+    <div class="px-5">
+        <h1>Temporary membership registration</h1>
+        <?php if (isset($_POST['submit']) && count($errors) === 0) : ?>
+            <!-- registered  -->
+            <p><?= $message ?></p>
+            <p>↓TEST(delete this later)：sent email has this URL</p>
+            <a href=" <?= $url ?>"><?= $url ?></a>
+        <?php else : ?>
+            <!-- register -->
+            <?php if (count($errors) > 0) : ?>
+                <?php
+                foreach ($errors as $value) {
+                    echo "<p class='error'>" . $value . "</p>";
+                }
+                ?>
+            <?php endif; ?>
+            <form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>" method="post">
+                <p>email address：<input type="text" name="mail" size="50" value="<?php if (!empty($_POST['mail'])) {
+                                                                                        echo $_POST['mail'];
+                                                                                    } ?>"></p>
+                <input type="hidden" name="token" value="<?= $token ?>">
+                <input type="submit" name="submit" value="Register">
+            </form>
+    </div>
+    </body>
+
+</html>
 <?php endif; ?>
